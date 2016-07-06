4a4b3a6ac381b6f7071be7e92fa207b871119d77
NicholasCK <nckoretski@gmail.com>
2016-07-06 18:11:01 -0400

    update relax

---
+++ Week 4/Class 07/class07/first-javascript/script.js
@@ -0,0 +1,28 @@
+alert("Ahoy mate");
+
+var num1 = 10 * 10;
+var num2= 100 / 10;
+var num3= 50 - 17;
+var num4= 50 + 17;
+
+	document.write((num1) + '<br/>');
+	document.write((num2) + '<br/>');
+	document.write((num3) + '<br/>'); 
+	document.write((num4) + '<br/>');
+	document.write((" Nick " + " Koretski ") + '<br/>');
+
+function doSomething () { 
+alert ("Hola");
+}
+
+doSomething();
+
+document.getElementById('greeting') .style.color = "red";
+
+var myPara=document.getElementById('myPara');
+
+myPara.style.color = "green";
+myPara.onclick = doSomething;
+
+
+
