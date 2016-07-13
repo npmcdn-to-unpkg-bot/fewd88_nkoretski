5bd48b44f40fadcefd4b222f346e80af0b11021e
NicholasCK <nckoretski@gmail.com>
2016-07-13 18:53:31 -0400

    commit

---
+++ Week 5/class10/jquery-review/app.js
@@ -0,0 +1,15 @@
+$('.box1').css('background-color' , 'yellow');
+
+
+
+
+
+$('.box2').css('background-color' , 'orange');
+
+$('.box2').click(function(){
+	$('.box2').text('Are my intials');    
+});
+
+
+
+
